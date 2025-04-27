b172a6b4955a7842178d38694810d50cd320815a
Arkady <orkasha@gmail.com>
2025-04-27 19:00:15 +0300

    refactor: routes, services layer add

---
+++ app/core/config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    url: str = "https://repo1.maven.org/maven2"
+    url1: str = "https://repo.maven.apache.org/maven2"
+    storage_path: str = "/tmp/storage"
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
--- app/main.py
+++ app/main.py
@@ -4,12 +4,13 @@ from fastapi.responses import JSONResponse
 from typing import Optional
 import httpx
 import hashlib
-
-from app.config import settings
-
+from app.routes.proxy_router import router as proxy_routes
+from app.core.config import settings
+from app.core.logger import setup_logging
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    setup_logging()
     print(f"🚀 Starting app with URL: {settings.url}")
     yield
     print("🛑 App shutting down.")
@@ -18,34 +19,4 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to RepoStore 🚀"}
-
-
-@app.api_route("/maven2/{full_path:path}", methods=["GET", "HEAD"])
-def handle_mypath(full_path: str, request: Request):
-    url = f"{settings.url}/{full_path}"
-    print(f"Request_IN from client: {full_path}. {request.method}. fwd url: {url}")
-    try:
-        with httpx.Client() as client:
-            response = client.get(url)
-            if request.method == "HEAD":
-                return Response(content=b"", headers=dict(response.headers))                
-            response_body = response.text
-            if response.headers.get("Content-Type", "") == "application/java-archive":
-                response_body = response.content
-            return Response(content=response_body, headers=dict(response.headers))
-    except httpx.RequestError as exc:
-        print(f"An error occurred while requesting {exc.request.url!r}: {str(exc)}")
-        return JSONResponse(
-            status_code=502,
-            content={"error": "Failed to fetch from url1", "details": str(exc)},
-        )
-
-    print("Unknown request type. {full_path}. {request.method}")
-    return {
-        "requested_subpath": full_path,
-        "method": request.method,
-        "info": "Request unsupported",
-    }
+app.include_router(proxy_routes)
